fa8586df2ac25499d3fad17b9d10d64e3b25111f
Saeed <104280610+derSaeed99@users.noreply.github.com>
2023-11-12 19:42:23 +0100

    fixup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 // import {onCall} from "firebase-functions/v2/https";
 //  import {onDocumentWritten} from "firebase-functions/v2/firestore";
 
@@ -11,3 +13,24 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // })
+
+
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+export const incrementUserCount = functions.firestore
+  .document("users/{userId}")
+  .onCreate(async (snap,) => {
+    const countersRef = firestore.collection("counters").doc("userCount");
+    const userDocRef = snap.ref;
+    const userDoc = snap.data();
+    const userCount = userDoc?.userNumber || 0;
+    await firestore.runTransaction(async (transaction) => {
+      const doc = await transaction.get(countersRef);
+      const count = doc.exists ? doc.data()?.userNumber : 0;
+      transaction.set(countersRef, {count: count + userCount});
+      transaction.update(userDocRef, {userNumber: userCount});
+    });
+  });
+
